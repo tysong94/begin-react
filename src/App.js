1fc531f9a3d90b68c29cb07796dddfde7a5991f2
tysong94 <tysong94@oniontech.com>
2020-12-20 14:28:24 +0900

    JSX 문법 숙지(javascript, css, 주석)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '24',
+    padding: '1rem'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>      
+      {/*주석 사용할 때.*/}
+      <Hello/>
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
     </div>
   );
 }
